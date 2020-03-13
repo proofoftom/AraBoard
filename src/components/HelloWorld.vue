2326c33656c33f1cdaf232b56a1899658efcd1b4
Tommy Cox <tommy.cox@protonmail.com>
2020-03-13 03:15:34 -0600

    A couple different graph types

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,18 +4,41 @@
       <v-btn @click="updateChart">Update!</v-btn>
     </v-row>
     <v-row>
-      <v-col v-for="n in 4" :key="n">
-        <v-card>
-          <v-card-title>Stat {{ n }}</v-card-title>
-          <div class="display-1 pa-3">+76%</div>
+      <v-col class="col-xs-12 col-sm-4 col-md-3" v-for="n in 4" :key="n">
+        <v-card style="min-width:176px">
+          <div style="right: 0; position:absolute;" class="display-1 pa-3">
+            +{{ series[0].data[2] }}%
+          </div>
+          <v-card-title style="position:absolute">Stat {{ n }}</v-card-title>
+          <apexchart
+            style="padding-top:30px;"
+            type="line"
+            :options="{
+              ...chartOptions,
+              chart: {
+                toolbar: {
+                  show: false
+                }
+              }
+            }"
+            :series="[series[0]]"
+          >
+          </apexchart>
         </v-card>
       </v-col>
     </v-row>
     <v-row>
-      <v-col class="col-xs-12 col-sm-6 col-md-4" v-for="n in 16" :key="n">
+      <v-col class="col-xs-12 col-sm-6 col-md-4" v-for="n in 6" :key="n">
         <v-card class="graph">
-          <v-card-title class="py-1">Multi-stat {{ n }}</v-card-title>
-          <apexchart type="area" :options="chartOptions" :series="series" />
+          <v-card-title style="position:absolute">
+            Multi-stat {{ n }}
+          </v-card-title>
+          <apexchart
+            style="padding-top:20px;"
+            type="area"
+            :options="chartOptions"
+            :series="series"
+          />
         </v-card>
       </v-col>
     </v-row>
@@ -27,9 +50,6 @@ export default {
   data: function() {
     return {
       chartOptions: {
-        chart: {
-          id: "vuechart-example"
-        },
         xaxis: {
           categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
         },
